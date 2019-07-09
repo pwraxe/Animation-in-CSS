2cc121c5132828ae6eba115d609b9bb89f9de9a5
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2019-07-09 23:01:00 +0530

    Update and rename Move_box_from_rectangular_corner.php to Move_box_from_rectangular_corner_alternate.php

--- Move_box_from_rectangular_corner_alternate.php
+++ Move_box_from_rectangular_corner_alternate.php
@@ -12,21 +12,34 @@
 			
 				animation-name: myAni;
 				animation-duration: 5s;
+				  
+				animation-direction: alternate;  
+				animation-timing-function:ease-in-out;
+				animation-iteration-count: infinite;
+				animation-fill-mode: backwards;
+				animation
+				
 			}
 			@keyframes myAni{
 				from{
+					background-color: red;
 					transform: translate(0px,0px);
 				}
 				25%{
+					background-color: blue;
 					transform: translate(1150px,0px);
 				}
 				50%{
+					background-color: black;
 					transform: translate(1150px,450px);
+					
 				}
 				75%{
+					background-color: green;
 					transform: translate(0px,450px);
 				}
 				to{
+					background-color: red;
 					transform: translate(0px,0px);
 				}
 
